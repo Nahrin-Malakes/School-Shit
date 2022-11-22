bfd21cf24c16517dbaa80e2000fa34c0fd4f4472
Nahrin Malakes <nahrinmalakes12@gmail.com>
2022-11-22 09:50:18 +0200

    Add files via upload

---
+++ client-server/Client/Program.cs
@@ -0,0 +1,176 @@
+﻿using System; //EventHandler
+using System.Windows.Forms; //Form
+using System.Drawing; //Point
+using System.Drawing.Text; //PrivateFontCollection
+using System.Text; //Encoding
+using System.IO; //File
+using System.Net; //IPAddress, IPEndpoint
+using System.Net.Sockets; //UdpClient
+using System.Threading; //Sleep
+using System.Net.NetworkInformation; //Ping
+using System.Drawing.Drawing2D; //GraphicsPath
+
+//класс CircularButton
+class CircularButton : Button
+{
+    protected override void OnPaint(PaintEventArgs pevent)
+    {
+        GraphicsPath gp = new GraphicsPath();
+        gp.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+        this.Region = new Region(gp);
+        base.OnPaint(pevent);
+    }
+}
+
+//class My1Form
+class My1Form : Form
+{
+    private CircularButton btn1, btn2;
+    private Label lb;
+    private TextBox tbox1, tbox2;
+
+    //1 constructor
+    public My1Form()
+    {
+        //form
+        ClientSize = new Size(280, 240); //size of my form
+        Text = "Client";
+        StartPosition = FormStartPosition.Manual;
+        Left = 1330;
+        Top = 20;
+        BackColor = Color.FromArgb(51, 190, 255);
+        ControlBox = false;
+        MinimizeBox = false;
+        MaximizeBox = false;
+
+        //button 1
+        btn1 = new CircularButton();
+        btn1.Enabled = true;
+        btn1.Text = "Delete";
+        btn1.Font = new Font("Comic Sans MS", 11.9F, FontStyle.Bold, GraphicsUnit.Point);
+        btn1.Size = new Size(65, 65);
+        btn1.FlatAppearance.BorderSize = 0;
+        btn1.FlatStyle = FlatStyle.Flat;
+        btn1.BackColor = Color.Red;
+        btn1.FlatAppearance.BorderColor = Color.Red; //border color
+        btn1.ForeColor = Color.Yellow;
+        btn1.Location = new Point(55, 160); //button1 coordinates
+        Controls.Add(btn1);
+        btn1.Click += new EventHandler(btn1_Click);
+
+        //button 2
+        btn2 = new CircularButton();
+        btn2.Enabled = true;
+        btn2.Text = "Exit";
+        btn2.Font = new Font("Comic Sans MS", 11.9F, FontStyle.Bold, GraphicsUnit.Point);
+        btn2.Size = new Size(65, 65);
+        btn2.FlatAppearance.BorderSize = 0;
+        btn2.FlatStyle = FlatStyle.Flat;
+        btn2.BackColor = Color.Blue;
+        btn2.FlatAppearance.BorderColor = Color.Blue; //border color
+        btn2.ForeColor = Color.Yellow;
+        btn2.Location = new Point(180, 160); //button2 coordinates
+        Controls.Add(btn2);
+        btn2.Click += new EventHandler(btn2_Click);
+
+        //label
+        lb = new Label();
+        lb.AutoSize = true;
+        //pfc.Families[0]: AlexBrush-Regular
+        lb.Font = new Font("Arial", 20F, FontStyle.Regular);//0,1,7
+        lb.Location = new Point(5, 1); //coordinates of the question
+        lb.Size = new Size(352, 24);
+        lb.Text = "Protocol UDP";
+        lb.ForeColor = Color.FromArgb(255, 17, 134);
+        Controls.Add(lb);
+
+        //textbox1 
+        tbox1 = new TextBox();
+        tbox1.Location = new Point(65, 65); //textbox location
+        tbox1.Size = new Size(160, 20); //textbox size
+        tbox1.BackColor = SystemColors.GradientActiveCaption;
+        tbox1.ForeColor = Color.Black; //font color
+        Font myfont = new Font("Arial", 16.0f); //font size
+        tbox1.Font = myfont;
+        tbox1.RightToLeft = RightToLeft.No;
+        tbox1.BorderStyle = BorderStyle.FixedSingle;
+        //tbox1.Text = "192.168.1.104"; //this is a server IP (or 127.0.0.1 for cumputer without network)
+        tbox1.Text = "127.0.0.1";
+        Controls.Add(tbox1);
+
+        //textbox2
+        tbox2 = new TextBox();
+        tbox2.Location = new Point(65, 115); //textbox location
+        tbox2.Size = new Size(160, 20); //textbox size
+        tbox2.BackColor = SystemColors.GradientActiveCaption;
+        tbox2.ForeColor = Color.Black; //font color
+        myfont = new Font("Arial", 16.0f); //font size
+        tbox2.Font = myfont;
+        tbox2.RightToLeft = RightToLeft.No;
+        tbox2.BorderStyle = BorderStyle.FixedSingle;
+        tbox2.Text = "a.txt"; //name of the file
+        Controls.Add(tbox2);
+    }
+
+    //2 button1
+    private void btn1_Click(object sender, EventArgs e)
+    {
+        if (IsConnectedToInternet())
+        {
+            string ipAddress = tbox1.Text; //this is a server ip
+            int port = 15000; //this is a server port
+            string nameOfFile = tbox2.Text; //this is a name of the file
+
+            //send name of the file to server
+            int theNumberOfBytes = udpSend(ipAddress, port, nameOfFile);
+            MessageBox.Show("Name of the file " + nameOfFile + " has been send..");
+        }
+        else
+            MessageBox.Show("Check your internet connection..");
+    }
+
+    //3 button2 
+    private void btn2_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    //4 udpSend
+    private int udpSend(string ipAddress, int port, string myMessage)
+    {
+        byte[] myMessageBytes = Encoding.UTF8.GetBytes(myMessage);
+        UdpClient udp = new UdpClient();
+        IPAddress ipad = IPAddress.Parse(ipAddress);
+        IPEndPoint ipen = new IPEndPoint(ipad, port);
+        int send = udp.Send(myMessageBytes, myMessageBytes.Length, ipen);
+        udp.Close();
+        return send;
+    }
+
+    //5 IsConnectedToInternet
+    public bool IsConnectedToInternet()
+    {
+        try
+        {
+            Ping myPing = new Ping();
+            String host = "google.com";
+            byte[] buffer = new byte[32];
+            int timeout = 1000;
+            PingOptions pingOptions = new PingOptions();
+            PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+            return (reply.Status == IPStatus.Success);
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    //11 Main 
+    [STAThread]
+    static void Main()
+    {
+        My1Form f = new My1Form();
+        Application.Run(f);
+    }
+}
---
+++ client-server/Server/Program.cs
@@ -0,0 +1,146 @@
+﻿using System; //Exception, Environment, EventHandler
+using System.Windows.Forms; //Label, MessageBox
+using System.Drawing; //Color, Font Size, Point
+using System.IO; //File
+using System.Text; //Encoding
+using System.Diagnostics; //Process
+using System.Threading; //Thread
+using System.Net.Sockets; //UdpClient
+using System.Net; //IPEndPoint, udp
+using System.Drawing.Text; //PrivateFontCollection
+
+//класс MyForm
+class MyForm : Form
+{
+    //кнопки и лейбл
+    private Button btn1, btn2;
+    private Label lb;
+
+    //сокет
+    private Thread rec;
+    private UdpClient udp;
+    private bool stopReceive;
+
+    //1. конструктор
+    public MyForm()
+    {
+        //сокет
+        rec = null;
+        udp = new UdpClient(15000); //15000 - порт сервера
+        stopReceive = false;
+
+        //форма
+        ClientSize = new System.Drawing.Size(290, 200); //size of my form
+        Text = "Server";
+        ControlBox = true; //false - remove all the Control Buttons (e.g. Minimize, Maximize, Exit) and also the icon
+        StartPosition = FormStartPosition.Manual;
+        BackColor = Color.FromArgb(178, 255, 51);
+        ShowInTaskbar = false;
+        Left = 1305; //координаты формы
+        Top = 30; //координаты формы
+        ControlBox = false;
+        MinimizeBox = false;
+        MaximizeBox = false;
+
+        //кнопка 1
+        btn1 = new Button();
+        btn1.Enabled = true;
+        btn1.Text = "Start";
+        btn1.Font = new Font("Arial", 10.5F, FontStyle.Bold, GraphicsUnit.Point);
+        btn1.Size = new Size(50, 30);
+        btn1.FlatAppearance.BorderSize = 0;
+        btn1.FlatStyle = FlatStyle.Flat;
+        btn1.BackColor = Color.Red;
+        btn1.FlatAppearance.BorderColor = Color.Red; //border color
+        btn1.ForeColor = Color.White;
+        btn1.Location = new Point(70, 110); //coordinates of my button
+        Controls.Add(btn1);
+        btn1.Click += new EventHandler(btn1_Click);
+
+        //кнопка 2
+        btn2 = new Button();
+        btn2.Enabled = true;
+        btn2.Text = "Stop";
+        btn2.Font = new Font("Arial", 10.5F, FontStyle.Bold, GraphicsUnit.Point);
+        btn2.Size = new Size(50, 30);
+        btn2.FlatAppearance.BorderSize = 0;
+        btn2.FlatStyle = FlatStyle.Flat;
+        btn2.BackColor = Color.SeaGreen;
+        btn2.FlatAppearance.BorderColor = Color.SeaGreen; //border color
+        btn2.ForeColor = Color.White;
+        btn2.Location = new Point(180, 110); //coordinates of my button
+        Controls.Add(btn2);
+        btn2.Click += new EventHandler(btn2_Click);
+
+        //лейбл 
+        lb = new Label();
+        lb.AutoSize = true;
+        lb.Font = new System.Drawing.Font("Arial", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        lb.Location = new System.Drawing.Point(1, 15); //coordinates of the question
+        lb.Size = new System.Drawing.Size(352, 24);
+        lb.Text = "Server Delete File";
+        lb.ForeColor = Color.FromArgb(144, 12, 63);
+        Controls.Add(lb);
+    }
+
+    //2. обработчик первой кнопки Start
+    private void btn1_Click(object sender, EventArgs e)
+    {
+        stopReceive = false;
+        rec = new Thread(new ThreadStart(ReceiveMessage));
+        rec.Start();
+        lb.Text = "Started listening";
+        lb.Location = new System.Drawing.Point(20, 15); //coordinates of the question
+    }
+
+    //3. обработчик второй кнопки Stop
+    private void btn2_Click(object sender, EventArgs e)
+    {
+        lb.Text = "Stop";
+        lb.Location = new System.Drawing.Point(110, 15);
+        stopReceive = true;
+        if (udp != null) udp.Close();
+        if (rec != null) rec.Join();
+        Close();
+    }
+
+    //4. функция ReceiveMessage для передачи процессу
+    void ReceiveMessage()
+    {
+        try
+        {
+            while (true)
+            {
+                //получаем данные от клиента
+                IPEndPoint ipendpoint = null;
+                byte[] message = udp.Receive(ref ipendpoint);//ipendpoint.Address.ToString() = 192.168.1.199 - ip клиента
+                //ipendpoint.Port.ToString() = 64696 - порта клиента
+                MessageBox.Show("N=" + message.Length);
+
+                //for example, mymessage = "a.txt"
+                string mymessage = "";
+                for (int i = 0; i < message.Length; i++)
+                    mymessage += (char)message[i];
+
+                //delete file a.txt
+                File.Delete("D:\\NahrinM\\" + mymessage);
+                Thread.Sleep(1500);
+                MessageBox.Show("File " + mymessage + " has been deleted..");
+
+                if (stopReceive == true) break;
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.Message);
+        }
+    }
+
+    //5. Главный модуль 
+    [STAThread]
+    static void Main()
+    {
+        MyForm f = new MyForm();
+        Application.Run(f);
+    }
+}
